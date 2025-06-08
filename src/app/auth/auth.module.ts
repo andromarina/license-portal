4d4ce109277c003193d4febc4c7b21214abf4316
MarkAvrelij <m.chuguy@gmail.com>
2025-06-08 17:06:31 +0200

    add loading spinner to login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -8,9 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
 import { authReducer } from './store/auth.reducer';
-import { AuthEffects } from './store/auth.effects';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthGuard } from './guards/auth.guard';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
@@ -23,6 +22,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatSnackBarModule,
     MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
